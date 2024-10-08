b5464f390a97312ed6f90d41c9c654a36b651ddd
Damon S. Green II <damonsgreen2@gmail.com>
2024-10-08 11:50:24 -0400

    lock on checks to ensure target is visible

--- Adversitas/Assets/Scripts/cameraLookController.cs
+++ Adversitas/Assets/Scripts/cameraLookController.cs
@@ -7,26 +7,25 @@ public class cameraLookController : MonoBehaviour, ILook
     [Header("-----Components-----")]
     [SerializeField] PlayerInput playerInput;
     [SerializeField] public Camera playerCamera;
+    [SerializeField] public Transform centerOfMass { get; set; }
+
 
     [Header("-----Normal Camera Factors-----")]
     [SerializeField] public cameraSettings normalCameraSettings;
 
     [Header("-----Combat Camera Factors-----")]
     [SerializeField] public cameraSettings combatCameraSettings;
+    [SerializeField] public float lockOnDistance = 5f; // Distance from the target when locked on
 
     [Header("-----Camera Mode Change Speed-----")]
     [SerializeField] public float cameraChangeSpeed;
-
-    [Header("-----LockOn-----")]
-    [SerializeField] private Transform lockOnTarget; // The target to lock onto
-    [SerializeField] private float lockOnDistance = 5f; // Distance from the target when locked on
-    private bool isLockedOn = false; // Whether the camera is currently locked on
 
     private Vector2 lookInput;
     private float currentYaw;
     private float currentPitch;
     private Transform playerPosition;
     private cameraSettings settings;
+    private lockOnController lockOnController;
 
 
 
@@ -34,28 +33,25 @@ public class cameraLookController : MonoBehaviour, ILook
     {
         playerInput.actions["Look"].performed += ctx => lookInput = ctx.ReadValue<Vector2>();
         playerInput.actions["Look"].canceled += ctx => lookInput = Vector2.zero;
-        playerInput.actions["LockOn"].performed += ctx => ToggleLockOn();
+        playerInput.actions["LockOn"].performed += ctx => lockOnController.ToggleLockOn();
 
         playerPosition = playerManager.instance.player.transform;
         settings = normalCameraSettings;
+
+        lockOnController = gameObject.AddComponent<lockOnController>();
+        lockOnController.Initialize(playerPosition, playerCamera, lockOnDistance, normalCameraSettings);
     }
 
     void Update()
     {
-        if (!isLockedOn)
+        if (!lockOnController.isLockedOn)
         {
             Look();
         }
         else
         {
-            if (lockOnTarget == null)
-            {
-                FindCameraTarget();
-            }
-           UpdateCombatCamera();
+            lockOnController.UpdateCombatCamera(settings);
         }
-        
-
         Debug.DrawRay(playerCamera.transform.position, playerCamera.transform.forward *50, Color.green);
     }
 
@@ -77,100 +73,6 @@ public class cameraLookController : MonoBehaviour, ILook
         Quaternion rotation = Quaternion.Euler(currentPitch, currentYaw, 0);
         Vector3 offset = rotation * new Vector3(0, 0, -settings.distanceFromPlayer) + new Vector3(0, settings.height, 0);
         playerCamera.transform.position = transform.position + offset;
-
         playerCamera.transform.LookAt(transform.position + Vector3.up * settings.height);
     }
-
-    public void UpdateCombatCamera()
-    {
-        //Quaternion lockOnRotation = Quaternion.Euler(currentPitch, currentYaw, 0);
-        //Vector3 lockOnOffset = lockOnRotation * new Vector3(0, 0, -settings.distanceFromPlayer) + new Vector3(0, settings.height, 0);
-        ////playerCamera.transform.position = playerCamera.transform.position + lockOnOffset;
-
-        if (lockOnTarget != null)
-        {
-            playerCamera.transform.LookAt(lockOnTarget.position + Vector3.up * settings.height);
-        }
-        else
-        {
-            playerCamera.transform.LookAt(transform.position + Vector3.up * settings.height);
-        }
-    }
-
-    public void ToggleLockOn()
-    {
-        isLockedOn = !isLockedOn;
-        Debug.Log("Locked on = " + isLockedOn);
-
-        if (isLockedOn)
-        {
-
-            if (lockOnTarget == null)
-            {
-                FindCameraTarget();
-                //CombatCameraOn();
-            }   
-
-        }
-        else
-        {
-            lockOnTarget = null;
-            NormalCameraOn();
-        }
-    }
-
-    public void FindCameraTarget()
-    {
-        Collider[] targets = Physics.OverlapSphere(playerPosition.position, lockOnDistance);
-
-        if (targets.Length <= 0)
-        {
-            return;
-        }
-
-        if (isLockedOn && lockOnTarget != null)
-        {
-            SwitchTarget();
-            return;
-        }
-
-        Debug.Log("Searching for targets");
-        for (int i = 0; i < targets.Length; i++)
-        {
-            GameObject parent = targets[i].gameObject;
-            LockOnMe enemy;
-            if (parent.TryGetComponent<LockOnMe>(out enemy)) {
-                enemy.CameraFollowsMe(playerCamera);
-                lockOnTarget = enemy.lockOnPosition;
-                CombatCameraOn();
-                UpdateCombatCamera();
-                Debug.Log(enemy + " is the new target");
-                return;
-            }
-
-        }
-    }
-
-    public void SwitchTarget()
-    {
-
-    }
-
-    public void NormalCameraOn()
-    {
-        settings = normalCameraSettings;
-        Rigidbody rb = playerCamera.GetComponent<Rigidbody>();
-        rb.isKinematic = true;
-        Physics.SyncTransforms();
-    }
-
-    public void CombatCameraOn()
-    {
-        settings = combatCameraSettings;
-        Rigidbody rb = playerCamera.GetComponent<Rigidbody>();
-        rb.isKinematic = false;
-        Physics.SyncTransforms();
-    }
-
-
 }
--- Adversitas/Assets/Scripts/lockOnController.cs
+++ Adversitas/Assets/Scripts/lockOnController.cs
@@ -4,15 +4,117 @@ using UnityEngine;
 
 public class lockOnController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("-----LockOn-----")]
+    public Transform lockOnTarget; // The target to lock onto
+    private float lockOnDistance = 5f; // Distance from the target when locked on
+    public bool isLockedOn = false; // Whether the camera is currently locked on
+    private Transform playerPosition;
+    private Camera playerCamera;
+    private cameraSettings normalSettings;
+
+    public void Initialize(Transform playerPosition, Camera playerCamera, float maxDistance, cameraSettings normal)
     {
-        
+        this.playerPosition = playerPosition;
+        this.playerCamera = playerCamera;
+        lockOnDistance = maxDistance;
+        normalSettings = normal;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ToggleLockOn()
     {
-        
+        isLockedOn = !isLockedOn;
+        Debug.Log("Locked on = " + isLockedOn);
+
+        if (isLockedOn)
+        {
+            if (lockOnTarget == null)
+            {
+                FindCameraTarget();
+            }
+        }
+        else
+        {
+            ResetLockOn();
+        }
+    }
+
+    public void UpdateCombatCamera(cameraSettings settings)
+    {
+        if (lockOnTarget != null)
+        {
+            playerCamera.transform.LookAt(lockOnTarget.position + Vector3.up * settings.height);
+        }
+        else
+        {
+            playerCamera.transform.LookAt(playerPosition.position + Vector3.up * settings.height);
+        }
+    }
+
+
+
+    /// Performs based on foot position. 
+    /// Needs to be given a solid center of mass position to help alleviate possible targeting errors
+    /// Still functional
+    public void FindCameraTarget()
+    {
+        Collider[] targets = Physics.OverlapSphere(playerPosition.position, lockOnDistance);
+
+        if (targets.Length <= 0) return;
+
+        if (isLockedOn && lockOnTarget != null)
+        {
+            SwitchTarget();
+            return;
+        }
+
+        Debug.Log("Searching for targets");
+        for (int i = 0; i < targets.Length; i++)
+        {
+            GameObject parent = targets[i].gameObject;
+            LockOnMe enemy;
+            if (parent.TryGetComponent<LockOnMe>(out enemy))
+            {
+                if (IsTargetVisible(parent.transform))
+                {
+                    enemy.CameraFollowsMe(playerCamera);
+                    lockOnTarget = enemy.lockOnPosition;
+                    Debug.Log(enemy + " is the new target");
+                    return;
+                }
+                else
+                {
+                    Debug.Log(enemy + " is obscured and cannot be targeted");
+                }
+            }
+        }
+        if (lockOnTarget == null)
+        {
+            UpdateCombatCamera(normalSettings);
+            ToggleLockOn();
+        }
+    }
+
+    private bool IsTargetVisible(Transform target)
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(playerPosition.position, target.position, out hit, lockOnDistance))
+        {
+            if (hit.collider.gameObject.layer != 9)
+            {
+                Debug.Log("hit object was " + hit.collider.gameObject.name);
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void SwitchTarget()
+    {
+
+    }
+
+    public void ResetLockOn()
+    {
+        lockOnTarget = null;
     }
 }
