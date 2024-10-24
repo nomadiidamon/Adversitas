0c204ab745058209fa0b994f27049120963edf64
Damon S. Green II <damonsgreen2@gmail.com>
2024-10-24 12:00:44 -0400

    stamina and ability set up to limit and refine dashing again

---
+++ Adversitas/Assets/Scripts/IAbility.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Interactions;
+
+public interface IAbility
+{
+	void ActivateAbility();
+}
+	
+
+public class Ability : Monobehaviour, IAbility
+{
+	protected Action myAbility;
+
+    protected virtual void Start()
+	{
+        myAbility = BeginAbility();
+	}
+
+	private void ActivateAbility ()
+	{
+		myAbility?.Invoke();
+	};
+
+	protected abstract void BeginAbility();
+
+
+
+
+}
---
+++ Adversitas/Assets/Scripts/Stamina.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Stamina
+{
+
+	private float currStaminaPoints; { get; set; }
+	private float maxStaminaPoints; {  get; set; }
+    private float staminaRegenRate; { get; set; }
+
+    public Stamina()
+	{
+
+	}
+
+	public void ReduceStamina(float value)
+	{
+
+	}
+
+	public UpdateStaminaValues()
+	{
+		if (currStaminaPoints == null || maxStaminaPoints == 
+			null || staminaRegenRate == null)
+		{
+			Debug.Log("not enough values");
+			return;
+		}
+
+	}
+
+}
