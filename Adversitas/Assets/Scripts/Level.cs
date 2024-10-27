e8bf801bfe6e7402c15975a1c99f2a29b58b1a94
Damon S. Green II <damonsgreen2@gmail.com>
2024-10-26 23:26:04 -0400

    level and stat base

---
+++ Adversitas/Assets/Scripts/Level.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class Level
+{
+    public int m_Level; { get; set; }
+
+    public int m_currExperience; { get; set; }
+    public int m_expToNextLevel; { get; set; }
+    public float m_levelRate; {  get; set; }
+
+}
---
+++ Adversitas/Assets/Scripts/Stat.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Stat
+{
+
+	string m_name; {  get; set; }
+	Level m_level; { get; set; }
+
+    int firstLimitValue; { get; set; }
+    int secondLimitValue;{ get; set; }
+    float firstLimitRate; { get; set; }
+    float secondLimitRate;{ get; set; }
+
+    public Stat(string name, int level)
+	 :m_name(name), m_level(level)
+	{
+
+	}
+
+
+}
