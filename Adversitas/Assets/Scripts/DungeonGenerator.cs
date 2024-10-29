47242322364c6e3778aaaff965ad7b0ef568c96a
Damon S. Green II <damonsgreen2@gmail.com>
2024-10-29 18:20:51 -0400

    Item class and start of dungeon generation system

---
+++ Adversitas/Assets/Scripts/DungeonGenerator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+
+enum Shapes = { CAPSULE, SPHERE, CUBE, RECTANGLE, PRISM, CYLINDER, CEILING, DOME, TUNNEL, }
+
+
+public class Space
+{
+	Vector2 m_center;
+	float m_spaceWidth;
+	float m_spaceHeight;
+	float m_offset;
+
+}
+
+
+public class Chunk
+{
+	Space m_space;
+	Vector<Space> sections;
+}
+
+public class Room
+{
+	Space m_space;
+	Shapes roomShape;
+	Vector<Chunk> 
+	float m_roomWidth;
+	float m_roomHeight;
+}
+
+
+public class DungeonGenerator
+{
+	public bool corridorFirst = true;
+
+	public float canvasHeight;
+	public float canvasWidth;
+
+
+}
---
+++ Adversitas/Assets/Scripts/Item.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Item
+{
+	private string m_name {  get; set; }
+	enum ItemType m_type = {BASIC_ITEM, KEY_ITEM, MATERIAL_ITEM, EQUIPMENT_ITEM, CONSUMABLE_ITEM, HOT_KEY} { get; set; }
+	private int m_value { get; set; }
+	private string m_description {  get; set; }
+	
+
+	public Item(string name, int value, string description, ItemType type)
+	:
+	{
+
+	}
+
+
+}
+
+
+
