892ec2b1d01db04f2c8d6d7733e77be27dc80495
Damon S. Green II <damonsgreen2@gmail.com>
2024-11-12 14:09:28 -0500

    stat requirement class added for items

---
+++ Adversitas/Assets/Scripts/StatRequirement.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class StatRequirement
+{
+	int minLevel;
+	bool meetsRequirment = false;
+
+	public StatRequirement(Stat stat)
+	{
+		if (stat != null) { 
+			if (stat.m_level.value >= minLevel)
+			{
+				meetsRequirment = true;
+			}
+			else
+			{
+				meetsRequirment = false;
+			}
+
+		}
+	}
+}
