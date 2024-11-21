ffb807259391dcce656a3944f236f1f9d987c225
Damon S. Green II <damonsgreen2@gmail.com>
2024-11-21 13:35:31 -0500

    Item classes seperated

---
+++ Adversitas/Assets/Scripts/Consumable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Consumable : Item
+{
+    private delegate void m_effect();
+
+    public Consumable(string name, int value, string description, ItemType type)
+        : base(name, value, description, type)
+    {
+
+    }
+
+
+}
--- Adversitas/Assets/Scripts/DamageValue.cs
+++ Adversitas/Assets/Scripts/DamageValue.cs
@@ -1,24 +1,19 @@
 ﻿using System;
 
 
+//public enum PhysicalDamageType { PIERCING, SLASHING, BLUNT, NULL}
+//public enum MagicalDamageType { FIRE, LAVA, METAL, EARTH, WATER, ICE, AIR, ELECTRICITY, NULL}
+public enum DamageType { PHYSICAL_PIERCING, PHYSICAL_SLASHING, PHYSICAL_BLUNT, MAGIC_FIRE, MAGIC_LAVA, MAGIC_METAL, MAGIC_EARTH, MAGIC_WATER, MAGIC_ICE, MAGIC_AIR, MAGIC_ELECTRICITY, NULL }
 
-public enum PhysicalDamageType { PIERCING, SLASHING, BLUNT, NULL}
-public enum MagicalDamageType { FIRE, LAVA, METAL, EARTH, WATER, ICE, AIR, ELECTRICITY, NULL}
-
+[System.Serializable]
 public class DamageValue
 {
-	PhysicalDamageType physicalDamage;
-	MagicalDamageType magicalDamage;
+	DamageType damageType;
 	int pointsToTakeFromHealth { get; }
 
-	public DamageValue(MagicalDamageType magDamage, int points)
+	public DamageValue(DamageType type, int points)
 	{
-		magicalDamage = magDamage;
-		pointsToTakeFromHealth = points;
-	}
-	public DamageValue(PhysicalDamageType physDamage, int points)
-	{
-		physicalDamage = physDamage;
+        damageType = type;
 		pointsToTakeFromHealth = points;
 	}
 }
